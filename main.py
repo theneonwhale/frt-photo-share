15aad583879229e51b9c2b1728253484422ea54c
Denys Tantsiura <ghost0002501@gmail.com>
2023-05-10 22:34:56 +0300

    main.py added.

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+
+from src.conf.messages import WELCOME
+# from src.routes import pictures
+
+
+app = FastAPI()
+
+# app.include_router(pictures.router, prefix='/api')
+
+
+@app.get('/')
+def read_root():
+    return {'message': WELCOME}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
---
+++ src/conf/messages.py
@@ -0,0 +1,2 @@
+WELCOME = 'Welcome to Photo-Share by Fast Rabbit Team.'
+TOKEN_TYPE = 'bearer'
