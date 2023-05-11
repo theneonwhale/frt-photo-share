c4aca9fe4694c63274ee67e1062950fab47e9d82
Anton Holovin <54354764+Unfeir@users.noreply.github.com>
2023-05-11 14:39:51 +0300

    add images and tags model

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from src.database.db import get_db
+
+app = FastAPI()
+
+
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        # Make request
+        result = db.execute(text("SELECT 1")).fetchone()
+        if result is None:
+            raise HTTPException(status_code=500, detail="Database is not configured correctly")
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
