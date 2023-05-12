38a67ac24b8fa25c057a4d4e0e15a44a4faca9d4
Denys Tantsiura <ghost0002501@gmail.com>
2023-05-12 10:05:08 +0300

    Added user-routes and updated services-images.

--- src/services/images.py
+++ src/services/images.py
@@ -1,5 +1,9 @@
+import hashlib
+from typing import BinaryIO
+
 from cloudinary import CloudinaryImage
 import cloudinary
+# from cloudinary.api import resource as res
 from cloudinary.uploader import upload
 from cloudinary.utils import cloudinary_url
 
@@ -31,6 +35,27 @@ class CloudImage:
             {'color': "yellow", 'effect': "outline:15:200"}
         ]
     }
+
+    @staticmethod
+    def generate_name_avatar(email: str):
+        user_name = hashlib.sha256(email.encode('utf-8')).hexdigest()[:14]
+        return f'FRT-PHOTO-SHARE-AVATARS/{user_name}'
+    
+    @staticmethod
+    def get_url_for_avatar(public_id, res, clipping: tuple[int, int] = (120, 120)):
+            src_url = (
+                cloudinary
+                .CloudinaryImage(public_id)
+                .build_url(width=clipping[0], height=clipping[1], crop='fill', version=res(public_id).get('version'))
+                )
+
+    @staticmethod
+    def avatar_upload(file: BinaryIO, email: str, clipping: tuple[int, int] = (120, 120)) -> str:
+        avatar_id = CloudImage.generate_name_avatar(email)
+        # the public_id parameter is set according to the name of the current user and the FRT-PHOTO-SHARE folder:
+        upload_result = cloudinary.uploader.upload(file.file, public_id=avatar_id, overwrite=True)
+
+        return CloudImage.get_url_for_avatar(avatar_id, upload_result, clipping)
 
 
 cloud_image = CloudImage()
