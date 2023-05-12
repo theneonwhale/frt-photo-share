e4b046a7eca87a7c7fe8acfe682ce8a5e1d065e2
Denys Tantsiura <ghost0002501@gmail.com>
2023-05-12 07:43:43 +0300

    Some fixes example.env, main, messages, models

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 import uvicorn
 
-from src.conf.messages import WELCOME
+from src.conf.messages import *
 from src.database.db import get_db
 # from src.routes import pictures
 
@@ -14,17 +14,17 @@ app = FastAPI()
 # app.include_router(pictures.router, prefix='/api')
 
 
-@app.get("/api/healthchecker")
+@app.get('/api/healthchecker')
 def healthchecker(db: Session = Depends(get_db)):
     try:
         # Make request
-        result = db.execute(text("SELECT 1")).fetchone()
+        result = db.execute(text('SELECT 1')).fetchone()
         if result is None:
-            raise HTTPException(status_code=500, detail="Database is not configured correctly")
-        return {"message": "Welcome to FastAPI!"}
+            raise HTTPException(status_code=500, detail=MSC500_DATABASE_CONFIG)
+        return {'message': WELCOME_FASTAPI}
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=500, detail="Error connecting to the database")
+        raise HTTPException(status_code=500, detail=MSC500_DATABASE_CONNECT)
 
         
  @app.get('/')
@@ -32,5 +32,5 @@ def read_root():
     return {'message': WELCOME}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
