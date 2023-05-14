1e0877be17986518106d776f7a5bfc6d083e93e4
Denys Tantsiura <ghost0002501@gmail.com>
2023-05-14 10:05:28 +0300

    Added edit image-description and leave a comments.

--- src/routes/images.py
+++ src/routes/images.py
@@ -7,11 +7,11 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.orm import Session
 
 from src.conf.config import settings
-from src.conf.messages import MSC404_IMAGE_NOT_FOUND
+from src.conf.messages import MSC404_IMAGE_NOT_FOUND, MSC412_IMPOSSIBLE
 from src.database.db import get_db
 from src.database.models import Image, User
 from src.repository import images as repository_images
-from src.schemas import ImageModel, ImageResponse
+from src.schemas import ImageModel, ImageResponse, CommentModel
 from src.services.auth import AuthUser
 
 
@@ -77,6 +77,77 @@ async def remove_image(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
     
     return image
+
+
+# EDIT image...
+@router.put(
+            '/{image_id}', 
+            description=f'No more than {settings.limit_crit} requests per minute',
+            dependencies=[Depends(RateLimiter(times=settings.limit_crit, seconds=60))],
+            response_model=ImageResponse, tags=['image']
+            )
+async def update_image(
+                       body: ImageModel,
+                       image_id: int = Path(ge=1), 
+                       db: Session = Depends(get_db),
+                       current_user: User = Depends(authuser.get_current_user),
+                       credentials: HTTPAuthorizationCredentials = Security(security)
+                       ) -> Image:  
+
+    image = await repository_images.update_image(image_id, body, current_user, db)
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
+
+    return image
+
+
+# Leave a comment... patch? post!?! addition to post-create?  ... & put? 
+@router.post(
+             '/{image_id}/{user_email}', 
+             description=f'No more than {settings.limit_crit} requests per minute',
+             dependencies=[Depends(RateLimiter(times=settings.limit_crit, seconds=60))],
+             response_model=ImageResponse, tags=['image']
+             )
+async def to_comment(
+                     body: CommentModel,
+                     image_id: int = Path(ge=1),
+                     user_email: str = Path(),  # regex... Email
+                     db: Session = Depends(get_db),
+                     current_user: User = Depends(authuser.get_current_user),
+                     credentials: HTTPAuthorizationCredentials = Security(security)
+                     ) -> Optional[Image]:
+    if user_email != current_user.email:
+        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=MSC412_IMPOSSIBLE)
+    
+    image = await repository_images.to_comment(body, image_id, current_user, db)
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
+
+    return image
+
+
+# .... -
+'''
+@router.patch(
+              '/{image_id}/to_name', 
+              description=f'No more than {settings.limit_crit} requests per minute',
+              dependencies=[Depends(RateLimiter(times=settings.limit_crit, seconds=60))],
+              response_model=ImageResponse, tags=['image']
+              )
+async def change_name_image(
+                              body: CatToNameModel,
+                              image_id: int = Path(ge=1),
+                              db: Session = Depends(get_db),
+                              current_user: User = Depends(authuser.get_current_user),
+                              credentials: HTTPAuthorizationCredentials = Security(security)
+                              ) -> Optional[Image]:
+
+    image = await repository_images.change_name_image(body, image_id, current_user, db)
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
+
+    return image
+'''
 
 
 # https://github.com/uriyyo/fastapi-pagination
