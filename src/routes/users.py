ed57c5c22c98bb8164038ebd06ee8075adfa491a
Denys Tantsiura <ghost0002501@gmail.com>
2023-05-14 11:44:49 +0300

    Fixed user profile update.

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, UploadFile, File  # status
+from fastapi import APIRouter, Depends, File, HTTPException, status, UploadFile, File
 from sqlalchemy.orm import Session
 
+from src.conf.messages import MSC404_IMAGE_NOT_FOUND
 from src.database.db import get_db
 from src.database.models import User
 from src.repository import users as repository_users
-from src.schemas import UserDb
+from src.schemas import UserDb, UserModel
 from src.services.auth import AuthUser
 from src.services.images import CloudImage  # cloud_image
 
@@ -18,15 +19,17 @@ async def read_users_me(current_user: User = Depends(authuser.get_current_user))
     return current_user
 
 
-# TODO profile foe all users =>
 @router.get('/about_{user_id}', response_model=UserDb)
 async def read_about_user(
                           user_id: int,
                           current_user: User = Depends(authuser.get_current_user), 
                           db: Session = Depends(get_db)
-                          ) -> User:
+                          ) -> User:  # !!! not User, = + User.number_images !! ToFix TODO
     # ... add number of uploaded images, etc ...
     user = repository_users.get_user_by_id(user_id, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
+    
     user.number_images = repository_users.get_number_of_images_per_user(user.email, db)
     
     return user
@@ -34,11 +37,14 @@ async def read_about_user(
 
 @router.put(f'/{authuser.get_current_user.username}', response_model=UserDb)
 async def update_user_profile(
+                              body: UserModel,
                               current_user: User = Depends(authuser.get_current_user), 
                               db: Session = Depends(get_db)
                               ) -> User:
     
-    ...
+    current_user = repository_users.update_user(current_user.email, body, db)
+    if not current_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
     
     return current_user
 
@@ -53,5 +59,7 @@ async def update_avatar_user(
     src_url = CloudImage.avatar_upload(file.file, current_user.email)
 
     user = await repository_users.update_avatar(current_user.email, src_url, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MSC404_IMAGE_NOT_FOUND)
 
     return user
